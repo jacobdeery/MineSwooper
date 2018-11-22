42a072af348425439e388dce324cb38fca457f9f
Jonathan Parson <bepie09@gmail.com>
2018-11-22 16:23:57 -0500

    Debugging on the Keil

--- main.c
+++ main.c
@@ -20,6 +20,7 @@ typedef struct {
 } game_peripheral_message;
 
 uint32_t time_remaining = 50;
+const uint16_t perFreq = 1; 		// Frequency in Hz
 
 osMailQDef(q1, 10, game_peripheral_message);
 osMailQId q1_id;
@@ -32,6 +33,7 @@ void game_peripheral_manager(void const *arg) {
 	LPC_ADC->ADCR = (1 << 2) | (4 << 8) | (1 << 21);
 
 	while(1){
+		osDelay(8000/perFreq);
 		game_peripheral_message message = {Not_Pressed, Not_Pressed, None, 0}; 
 		
 		if(phase == Menu){
@@ -40,29 +42,36 @@ void game_peripheral_manager(void const *arg) {
 			while(!((LPC_ADC->ADGDR >> 31) & 0x1));
 			
 			message.pot = (uint32_t)((LPC_ADC->ADGDR >> 4) & 0xFFF);
+			printf("%u \n",message.pot);
+
 		} else if(phase == Game){ 
 			//Joystick
 			uint32_t joy = LPC_GPIO1->FIOPIN >> 20; 
 
 			//Joystick Button
 			if(!(joy & 0x1)) message.joyButton = Pressed;
-
+			
 			//Joystick Direction
-			if(!(joy>>23 & 0x1))
+			if(!(joy>>3 & 0x1)) { 
 				message.joyDirection = Left;
-			else if(!(joy>>24 & 0x1))
+			} else if(!(joy>>4 & 0x1)) {
 				message.joyDirection = Up;
-			else if(!(joy>>25 & 0x1)) 
+			} else if(!(joy>>5 & 0x1)) {
 				message.joyDirection = Right;
-			else if(!(joy>>26 & 0x1)) 
+			} else if(!(joy>>6 & 0x1)) { 
 				message.joyDirection = Down;
-			else 
+			} else { 
 				message.joyDirection = None;
+			}
+			
 		}
-		
-		//INT0 Button
-		message.blueButton = (uint32_t)(LPC_GPI02->FIOPIN >> 10);
-		
+
+		if((LPC_GPIO2->FIOPIN >> 10) & 0x1) { 
+			message.blueButton = Not_Pressed;
+		} else {
+			message.blueButton = Pressed; 
+		}
+
 		game_peripheral_message *p_message = osMailAlloc(q1_id, 0);
 		if (p_message != NULL) osMailPut(q1_id, p_message);
 
@@ -74,17 +83,18 @@ osThreadDef(game_peripheral_manager, osPriorityNormal, 1, 0);
 
 void game_logic_manager(void const *arg) {
 	phase = Menu; // game starts in main menu
+	
 	while(1) {
 		game_peripheral_message message; 
 		
 		osEvent evt = osMailGet(q1_id, osWaitForever);
 		
 		if(evt.status == osEventMail) {
-			game_peripheral_copy((game_peripheral_message *) evt.value.p, &message);
+			memcpy(&message, evt.value.p, sizeof(game_peripheral_message));
 			osMailFree(q1_id, evt.value.p);
 		}
 
-		if(phase == Menu) 
+		if(phase == Menu) {
 			if(message.blueButton == Pressed){
 				phase = Game; 
 				time_remaining = ((float)message.pot/4095)*50; 
@@ -110,30 +120,31 @@ void game_logic_manager(void const *arg) {
 				time_remaining = 50; 
 			}
 		}
-
-		osThreadYield();
+		osThreadYield(); 
 	}	
-	/*
-		NOTES: 
-			1. Clear the queue between game states? 
-			2. How fast should we be running peripherals to avoid "duplicate" actions? 	
-		STRUCTURE: 
-			1. Extract peripheral message from queue
-			2. Check game state 
-			3. Read peripheral message
-			4. Perform action
-			5. Update map structure
-		
-		PERIPHERAL PRECEDENCE 
-			INT0 > MOVE > BUTTON PUSH > POT
-			Need to discuss if we're going to allow "compound actions" or w.e. 
-	*/
 }
+
+/*
+	NOTES: 
+		1. Clear the queue between game states? 
+		2. How fast should we be running peripherals to avoid "duplicate" actions? 	
+	STRUCTURE: 
+		1. Extract peripheral message from queue
+		2. Check game state 
+		3. Read peripheral message
+		4. Perform action
+		5. Update map structure
+	
+	PERIPHERAL PRECEDENCE 
+		INT0 > MOVE > BUTTON PUSH > POT
+		Need to discuss if we're going to allow "compound actions" or w.e. 
+*/
 
 osThreadDef(game_logic_manager, osPriorityNormal, 1, 0);
 
 void display_manager(void const *arg) {
 	while(1) {
+		printf("%u \n", time_remaining);
 		if(phase == Menu) {
 			// display menu graphics
 			while(phase == Menu) {
@@ -160,17 +171,10 @@ void display_manager(void const *arg) {
 	}	
 }
 
-void game_peripheral_copy(game_peripheral_message *m1, game_peripheral_message *m2){
-	m2->blueButton = m1->blueButton;
-	m2->joyButton = m1->joyButton;
-	m2->joyDirection = m1->joyDirection;
-	m2->pot = m1->pot;
-}
-
 osThreadDef(display_manager, osPriorityNormal, 1, 0);
 
 int main() {
-	printf("Initializing...");
+	printf("Initializing... \n");
 
 	q1_id = osMailCreate(osMailQ(q1), NULL);
 
