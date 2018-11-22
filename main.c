be60cf28845c1a9a42a7f8c2cf0f6dca4386156b
Jonathan Parson <jonathanparson@Jonathans-MacBook-Pro.local>
2018-11-21 21:30:00 -0500

    Added pot stuff and message struct for peripherals

--- main.c
+++ main.c
@@ -2,6 +2,17 @@
 Jacob Deery (jbdeery) and Jonathan Parson (jmparson)
 */
 
+/* 
+	POTENTIOMETER NOTES: 
+	
+	0 is pressed, 1 is not pressed
+	
+	BITMAP: 
+	Left, Up, Right, Down, Button
+		23  24     25    26      20
+	
+*/
+
 #include <lpc17xx.h>
 #include "stdio.h"
 #include "GLCD.h"
@@ -9,28 +20,34 @@ Jacob Deery (jbdeery) and Jonathan Parson (jmparson)
 #include <string.h>
 
 enum gamePhase{Menu, Game, Victory, GameOver} phase;
+enum button{Pressed, Not_Pressed};
+enum joy{Up, Down, Left, Right};
+
+typedef struct {
+	enum button blueButton; 
+	enum button joyButton;
+	enum joy joyStick;
+	uint32_t pot; 
+} game_peripheral_message;
 
 void game_peripheral_manager(void const *arg) {
-	/* 
-		POTENTIOMETER NOTES: 
-		
-		0 is pressed, 1 is not pressed
-	 	
-		BITMAP: 
-		Left, Up, Right, Down, Button
-		  23  24     25    26      20
-	 	
-	*/
+	
+	//Pot config
+	LPC_PINCON->PINSEL1 &= ~(0x03<<18);
+	LPC_PINCON->PINSEL1 |= (0x01<<18);
+	LPC_SC->PCONP |= (0x1<<12);
+	LPC_ADC->ADCR = (1 << 2) | (4 << 8) | (1 << 21);
 
 	while(1){
 		if(phase == Menu){
-			//Potentiometer
-			uint32_t joy = LPC_GPIO1->FIOPIN >> 20; 
+			//ADC conversion 
+			LPC_ADC->ADCR |= (0x1<<24);
+			while(!((LPC_ADC->ADGDR >> 31) & 0x1));
 			
+			uint32_t pot = (LPC_ADC->ADGDR >> 4) & 0xFFF;
 		} else if(phase == Game){ 
 			// Joystick
 			uint32_t joy = LPC_GPIO1->FIOPIN >> 20; 
-			
 		}
 		
 		uint32_t button = LPC_GPI02->FIOPIN >> 10; //INT0 button is required in all states, so constantly check it
